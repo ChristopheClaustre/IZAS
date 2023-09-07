2476f4138522d86a5310ffb83fc2d0a1d9d2e546
chris.clst <christophe.claustre.31@gmail.com>
2023-09-07 23:13:41 +0200

    Update script.js - firebase

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAd_Cg6dKfDOwwanQyaCDVSzk0o6JR3VuI",
+    authDomain: "izas-6e7b5.firebaseapp.com",
+    projectId: "izas-6e7b5",
+    storageBucket: "izas-6e7b5.appspot.com",
+    messagingSenderId: "911743921374",
+    appId: "1:911743921374:web:d81c1ed25bc501b258e64f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
