93d43d988967df7fee1cd71e41b760fb96de267f
ChristopheClaustre <christophe.claustre.31@gmail.com>
2023-09-11 19:56:28 +0200

    Setup complete PJ and MJ view (not sync at the moment)

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,82 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize MJ page
+function initializeMJ() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: fenway,
+    zoom: 14,
+    fullscreenControl: false,
+    motionTracking: false,
+    motionTrackingControl: false
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+      fullscreenControl: false,
+      motionTracking: false,
+      motionTrackingControl: false
+    }
+  );
+
+  map.setStreetView(panorama);
+}
+
+window.initializeMJ = initializeMJ;
+
+// Initialize PJ page
+function initializePJ() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano-pj"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+      fullscreenControl: false,
+      motionTracking: false,
+      motionTrackingControl: false,
+      addressControl: false,
+      linksControl: false,
+      enableCloseButton: false,
+      clickToGo: false
+    }
+  );
+  
+  // To disable movement with keyboard
+  window.addEventListener(
+    'keydown',
+      (event) => {
+        if (
+          (
+            // Change or remove this condition depending on your requirements.
+               event.key === 'ArrowUp' // Move forward
+            || event.key === 'ArrowDown' // Move forward
+            /*||   event.key === 'ArrowLeft' // Pan left
+            ||   event.key === 'ArrowRight' // Pan right
+            ||   event.key === '+' // Zoom in
+            ||   event.key === '=' // Zoom in
+            ||   event.key === '_' // Zoom out
+            ||   event.key === '-' // Zoom out*/
+          ) &&
+          !event.metaKey &&
+          !event.altKey &&
+          !event.ctrlKey
+        ) {
+          event.stopPropagation()
+        };
+      },
+      { capture: true },
+    );
+}
+
+window.initializePJ = initializePJ;
