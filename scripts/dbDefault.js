db7d759da782cd95034570ae55d13ead8b9f43cb
ChristopheClaustre <christophe.claustre.31@gmail.com>
2023-09-17 01:00:45 +0200

    Store static information in separate files

---
+++ scripts/dbDefault.js
@@ -0,0 +1,6 @@
+export const data =
+{
+	"pegman" : { "lat": 43.604579144543436, "lng" : 1.443364389561114 }, /* Toulouse, place du capitole */
+	"resources" : { "confort" : 0, "foods" : 0, "heal" : 0 },
+	"player" : { "resistance" : { "current" : 4, "max" : 4}, "jobID" : 0 },
+}
